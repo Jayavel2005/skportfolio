a8f1a809222c1ef4c50eeec0b22c0dd9a047a31c
Jayavel <jayavel1520@gmail.com>
2024-10-22 09:32:54 +0530

    preloader and education timeline added

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-
-    document.addEventListener('DOMContentLoaded', function () {
-        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-            return new bootstrap.Tooltip(tooltipTriggerEl);
-        });
+// Tool Tip Animation
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
     });
+});
 
+
+// Loader
+
+const loader = document.getElementById("loader");
+window.addEventListener("load",()=>{
+    setTimeout(()=>{
+        loader.style.display = "none";
+    },2500)
+})
